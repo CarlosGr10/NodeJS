e6390fde94ac93d696f3b4fbeaca0e5a9f27b194
Carlos Garcia <tigrecarlos1@gmail.com>
2020-08-23 22:36:40 -0500

    Peticiones, acceso a metodos y rutas

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/',function(req,res){
-    res.send('hola');
+app.use(router);
+
+router.get('/mensaje', function(req, res){
+    res.send('Lista de mensajes')
 });
 
+router.post('/mensaje', function (req, res){
+    res.send('Mensaje añadido')
+});
+
+router.delete('/mensaje', function(req, res){
+    res.send('Mensaje eliminado')
+});
+/* app.use('/',function(req,res){
+    res.send('hola');
+});
+ */
 app.listen(3000);
 console.log('La aplicación esta en el puerto http//localhost:3000');
