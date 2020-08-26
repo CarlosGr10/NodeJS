251f5630cd31e3bcfcd53aac14ce6a81e424b7ae
Carlos Garcia <tigrecarlos1@gmail.com>
2020-08-25 22:13:41 -0500

    Presentar errores

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ router.get('/mensaje', function(req, res){
 router.post('/mensaje', function (req, res){
     console.log(req.query);
     if (req.query.error == "ok") {
-        response.error(req, res, 'Error simulado', 400);
+        response.error(req, res, 'Error inesperado', 500, 'Esto es solo una simulacion de los errores');
     } else {
         response.success(req, res, 'Mensaje creado correctamente', 201);
     }
